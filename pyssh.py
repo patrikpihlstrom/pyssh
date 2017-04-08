f9da325c32b836ff0b4d4553d4db1456fc417acb
Patrik Pihlström <patrik@crealevant.se>
2017-04-09 00:07:38 +0200

    fixed get_host

--- pyssh.py
+++ pyssh.py
@@ -27,7 +27,7 @@ def get_host(args):
     try:
         user_host = str(args).split("@")
         if len(user_host) == 2:
-            return re.findall(r"[\w]+", user_host[1])[0]
+            return re.sub('\']', '', user_host[1].split(' ')[0])
     except Exception:
         return args[1]
 
